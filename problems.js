e1ec7be6d35bfe729298659351dbe0c18a927fa6
ashraful-fahim <ashraful30june@gmail.com>
2025-02-18 23:34:07 +0600

    solved some problems on javascript

---
+++ problems.js
@@ -0,0 +1,142 @@
+function calculateVAT(price) {
+    if (typeof price === 'number' && price >= 0) {
+        const vatRate = 7.5 / 100;
+        const vat = price * vatRate;
+        return vat;
+    }
+    else {
+        return 'Invalid';
+    }
+}
+
+// const price = calculateVAT('foo');
+// console.log(price);
+
+
+
+
+function validContact(contact) {
+    if (typeof contact === 'string') {
+        const len = contact.length;
+        const first = contact[0];
+        const second = contact[1];
+        // console.log(len,first,second)
+        if (len !== 11 || first !== '0' || second !== '1' || contact.includes(' ') === true) {
+            return false;
+        }
+        else {
+            return true;
+        }
+    }
+    else {
+        return 'Invalid';
+    }
+}
+
+const price = vali('foo');
+console.log(price);
+
+
+
+function willSuccess(marks) {
+    const arr = Array.isArray(marks);
+    if (arr !== true) {
+        return 'Invalid';
+    }
+    else {
+        const pass = [];
+        const fail = [];
+        for (const mark of marks) {
+            if (mark >= 50) {
+                pass.push(mark);
+            }
+            else {
+                fail.push(mark)
+            }
+        }
+        const passLen = pass.length;
+        const failLen = fail.length;
+        if (passLen > failLen) {
+            return true;
+        }
+        else {
+            return false;
+        }
+    }
+}
+
+
+
+
+
+function validProposal(person1, person2) {
+    const arr1 = Array.isArray(person1);
+    const arr2 = Array.isArray(person2);
+    const ageDiff = Math.abs(person1.age - person2.age);
+    // console.log(ageDiff);
+    if (typeof person1 !== 'object' || typeof person2 !== 'object' || arr1 === true || arr2 === true) {
+        return 'Invalid';
+    }
+    else {
+        if (person1.gender !== person2.gender) {
+            if (ageDiff <= 7) {
+                return true;
+            }
+            else {
+                return false;
+            }
+        }
+        else {
+            return false;
+        }
+    }
+}
+
+// const person1 = { name: "toya", gender: "female", age: 35 };
+// const person2 = { name: "kader", gender: "male", age: 28 };
+// const per = validProposal(person1, person2);
+// console.log(per);
+
+
+function calculateSleepTime(times) {
+    const arr = Array.isArray(times);
+    if (arr !== true) {
+        return 'Invalid';
+    }
+    else {
+        let sec = 0;
+        for(const time of times){
+            if(typeof time === 'number'){
+                sec = sec + time;
+            }
+            else{
+                return 'Invalid';
+            }
+        }
+        const hour = sec / 3600;
+        const objHour = parseInt(hour);
+        // console.log(hour);
+        const remainingHourSec = hour - objHour;
+        // console.log(remainingHourSec);
+        const min = remainingHourSec * 60;
+        // console.log(min);
+        let objMin = parseInt(min);
+        const remainingMinSec = min - objMin;
+        // console.log(remainingMinSec);
+        let objSec = Math.round(remainingMinSec * 60);
+        if (objSec === 60){
+            objMin++;
+            objSec = 0;
+        }
+        const object = {
+            hour : objHour,
+            minute : objMin,
+            second : objSec,
+        }
+        return object;
+    }
+}
+
+// const times = [1000, 499, 519, 300];
+// const per = calculateSleepTime(times);
+// console.log(per);
---
+++ rough.js
@@ -0,0 +1,19 @@
+// const rough = '017780798 03'
+// if(rough.includes(' ') === true){
+//     console.log('ok');
+// }
+// else{
+//     console.log('wrong')
+// }
+
+const num1 = [234];
+const num2 = [234];
+
+const arr = Array.isArray(num1);
+const arr2 = Array.isArray(num2);
+if (arr === true && arr2 === true){
+    console.log(true);
+}
+else{
+    console.log(false);
+}
